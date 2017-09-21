d48c508551268aa98b5a3bcb56ebbead00b59ca6
Jonathan Rufino <jonathan.rufinotkd@gmail.com>
2017-09-20 23:24:39 -0300

    Create model to represent the baby crib

--- babyonboard/api/models.py
+++ babyonboard/api/models.py
@@ -26,3 +26,29 @@ class Breathing(models.Model):
 
     def __str__(self):
         return str(self.is_breathing)
+
+
+class BabyCrib(models.Model):
+    RESTING = 'Resting'
+    FRONT = 'Front'
+    SIDE = 'Side'
+    VIBRATION = 'Vibration'
+
+    MOVEMENT_CHOICES = (
+        (RESTING, 'resting'),
+        (FRONT, 'front'),
+        (SIDE, 'side'),
+        (VIBRATION, 'vibration'),
+    )
+
+    status = models.CharField(
+        max_length=10,
+        choices=MOVEMENT_CHOICES,
+        default=RESTING
+    )
+    duration = models.PositiveIntegerField()
+    date = models.DateField(auto_now=True, auto_now_add=False)
+    time = models.TimeField(auto_now=True, auto_now_add=False)
+
+    def __str__(self):
+        return str(self.status)
