37eb01b077018f7cf941bb232d8480122764518c
Felipe César Silveira de Assis <fel.cesar@gmail.com>
2017-09-29 12:33:11 -0300

    Sketch implementation of movement SET - separated GET and POST

--- babyonboard/api/views.py
+++ babyonboard/api/views.py
@@ -1,15 +1,22 @@
 from django.http import HttpResponse
 from .models import Temperature, HeartBeats, Breathing, BabyCrib
 from .utils import jsonify
+from django.core import serializers
 
 
 JSON_CONTENT = 'application/json'
 
 def temperature_now(request):
     temperature = Temperature.objects.order_by('date', 'time').last()
-    content = jsonify(temperature)
+    temperature = [temperature, ]
+    # content = jsonify(temperature)
+    # serialize queryset
+    if temperature is None:
+        temperature = []
+    serialized_queryset = serializers.serialize('json', temperature)
 
-    return HttpResponse(content, JSON_CONTENT)
+    # serialize object
+    return HttpResponse(serialized_queryset, JSON_CONTENT)
 
 def heartbeats_now(request):
     heartbeats = HeartBeats.objects.order_by('date', 'time').last()
@@ -23,8 +30,24 @@ def breathing_now(request):
 
     return HttpResponse(content, JSON_CONTENT)
 
+def movement(request):
+
+    if request.POST:
+        return movement_set(request)
+    elif request.GET:
+        return movement_now(request)
+
+    return HttpResponse('', status=405, reason='Method not allowed, only GET or POST.')
+
+
+# Private Methods
+
+def movement_set(request):
+    # Make python code to summon a C executable or a shell script that does it
+    return HttpResponse('', status=200)
+
+
 def movement_now(request):
     babycrib = BabyCrib.objects.order_by('date', 'time').last()
     content = jsonify(babycrib)
-
     return HttpResponse(content, JSON_CONTENT)
