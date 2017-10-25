4bfc2ee84fb518b216226608becd1d0aa75c3cd6
Jonathan Rufino <jonathan.rufinotkd@gmail.com>
2017-10-25 20:48:14 -0200

    Implement historical register api endpoints

--- babyonboard/api/urls.py
+++ babyonboard/api/urls.py
@@ -4,7 +4,19 @@ from . import views
 
 urlpatterns = [
     url(r'^temperature/$', views.temperature_now, name='temperature_now'),
+    url(r'^temperature/(?P<year>[0-9]{4})/$', views.temperature_year_archive),
+    url(r'^temperature/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/$', views.temperature_month_archive),
+    url(r'^temperature/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2})/$', views.temperature_day_archive),
+
     url(r'^heartbeats/$', views.heartbeats_now, name='heartbeats_now'),
+    url(r'^heartbeats/(?P<year>[0-9]{4})/$', views.heartbeats_year_archive),
+    url(r'^heartbeats/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/$', views.heartbeats_month_archive),
+    url(r'^heartbeats/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2})/$', views.heartbeats_day_archive),
+
     url(r'^breathing/$', views.breathing_now, name='breathing_now'),
+    url(r'^breathing/(?P<year>[0-9]{4})/$', views.breathing_year_archive),
+    url(r'^breathing/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/$', views.breathing_month_archive),
+    url(r'^breathing/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2})/$', views.breathing_day_archive),
+    
     url(r'^movement/$', views.movement, name='movement'),
 ]
--- babyonboard/api/views.py
+++ babyonboard/api/views.py
@@ -2,24 +2,72 @@ from django.http import HttpResponse, JsonResponse
 from .models import Temperature, HeartBeats, Breathing, BabyCrib
 from .utils import jsonify
 from .utils import runCScript
-from .serializers import TemperatureSerializer, HeartBeatsSerializer, BreathingSerializer
+from .serializers import TemperatureSerializer, HeartBeatsSerializer, BreathingSerializer, BabyCribSerializer
 import json
 
 
+# Temperatures endpoints
 def temperature_now(request):
     temperature = Temperature.objects.order_by('date', 'time').last()
     serializer = TemperatureSerializer(temperature)
     return JsonResponse(serializer.data)
 
+def temperature_year_archive(request, year):
+    temperatures = Temperature.objects.filter(date__year=year)
+    serializer = TemperatureSerializer(temperatures, many=True)
+    return JsonResponse(serializer.data, safe=False)
+
+def temperature_month_archive(request, year, month):
+    temperatures = Temperature.objects.filter(date__year=year, date__month=month)
+    serializer = TemperatureSerializer(temperatures, many=True)
+    return JsonResponse(serializer.data, safe=False)
+
+def temperature_day_archive(request, year, month, day):
+    temperatures = Temperature.objects.filter(date__year=year, date__month=month, date__day=day)
+    serializer = TemperatureSerializer(temperatures, many=True)
+    return JsonResponse(serializer.data, safe=False)
+
+# Heartbeats endpoints
 def heartbeats_now(request):
     heartbeats = HeartBeats.objects.order_by('date', 'time').last()
     serializer = HeartBeatsSerializer(heartbeats)
     return JsonResponse(serializer.data)
 
+def heartbeats_year_archive(request, year):
+    heartbeats = HeartBeats.objects.filter(date__year=year)
+    serializer = HeartBeatsSerializer(heartbeats, many=True)
+    return JsonResponse(serializer.data, safe=False)
+
+def heartbeats_month_archive(request, year, month):
+    heartbeats = HeartBeats.objects.filter(date__year=year, date__month=month)
+    serializer = HeartBeatsSerializer(heartbeats, many=True)
+    return JsonResponse(serializer.data, safe=False)
+
+def heartbeats_day_archive(request, year, month, day):
+    heartbeats = HeartBeats.objects.filter(date__year=year, date__month=month, date__day=day)
+    serializer = HeartBeatsSerializer(heartbeats, many=True)
+    return JsonResponse(serializer.data, safe=False)
+
+# Breathing endpoints
 def breathing_now(request):
     breathing = Breathing.objects.order_by('date', 'time').last()
     serializer = BreathingSerializer(breathing)
     return JsonResponse(serializer.data)
+
+def breathing_year_archive(request, year):
+    breathings = Breathing.objects.filter(date__year=year)
+    serializer = BreathingSerializer(breathings, many=True)
+    return JsonResponse(serializer.data, safe=False)
+
+def breathing_month_archive(request, year, month):
+    breathings = Breathing.objects.filter(date__year=year, date__month=month)
+    serializer = BreathingSerializer(breathings, many=True)
+    return JsonResponse(serializer.data, safe=False)
+
+def breathing_day_archive(request, year, month, day):
+    breathings = Breathing.objects.filter(date__year=year, date__month=month, date__day=day)
+    serializer = BreathingSerializer(breathings, many=True)
+    return JsonResponse(serializer.data, safe=False)
 
 def movement_now(request):
     babycrib = BabyCrib.objects.order_by('date', 'time').last()
