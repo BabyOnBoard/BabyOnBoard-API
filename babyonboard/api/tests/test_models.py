7603ea07d67cc325d3a9a5312cb9a32630618fe6
Jonathan Rufino <jonathan.rufinotkd@gmail.com>
2017-11-23 17:11:21 -0200

    Implement tests for heartbeats model

--- babyonboard/api/tests/test_models.py
+++ babyonboard/api/tests/test_models.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from ..models import Temperature
+from ..models import Temperature, HeartBeats
 
 
 class TemperatureTest(TestCase):
@@ -13,3 +13,16 @@ class TemperatureTest(TestCase):
         self.assertIsNotNone(temperature)
         self.assertTrue(isinstance(temperature, Temperature))
         self.assertEqual(temperature.__str__(), str(temperature.temperature))
+
+
+class HeartBeatsTest(TestCase):
+    """Test class for heartbeats model"""
+
+    def setUp(self):
+        HeartBeats.objects.create(beats=70)
+
+    def test_create_heartbeats(self):
+        heartBeats = HeartBeats.objects.get(beats=70)
+        self.assertIsNotNone(heartBeats)
+        self.assertTrue(isinstance(heartBeats, HeartBeats))
+        self.assertEqual(heartBeats.__str__(), str(heartBeats.beats))
