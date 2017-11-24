c28688324c2e89ab13841788bdcad20732857f99
Jonathan Rufino <jonathan.rufinotkd@gmail.com>
2017-11-24 10:00:48 -0200

    Implement tests for streaming endpoint

--- babyonboard/api/tests/test_views.py
+++ babyonboard/api/tests/test_views.py
@@ -134,3 +134,31 @@ class CreateNewBreathingTest(TestCase):
             content_type='application/json'
         )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+class ControlStreamingTest(TestCase):
+    """ Test class for starting or stoping streaming """
+
+    def setUp(self):
+        self.valid_payload = {
+            'action': 'start'
+        }
+        self.invalid_payload = {
+            'action': 'continue'
+        }
+
+    def test_valid_streaming_control(self):
+        response = client.post(
+            reverse('streaming'),
+            data=json.dumps(self.valid_payload),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_invalid_streaming_control(self):
+        response = client.post(
+            reverse('streaming'),
+            data=json.dumps(self.invalid_payload),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
