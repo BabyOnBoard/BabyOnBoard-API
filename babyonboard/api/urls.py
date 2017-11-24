61d4c38f16accabc2e0f7012c617a9adf4457485
Jonathan Rufino <jonathan.rufinotkd@gmail.com>
2017-11-24 09:26:01 -0200

    Implement breathing POST endpoint

--- babyonboard/api/urls.py
+++ babyonboard/api/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     url(r'^heartbeats/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/$', views.heartbeats_month_archive),
     url(r'^heartbeats/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2})/$', views.heartbeats_day_archive),
 
-    url(r'^breathing/$', views.breathing_now, name='breathing_now'),
+    url(r'^breathing/$', views.breathing, name='breathing'),
     url(r'^breathing/(?P<year>[0-9]{4})/$', views.breathing_year_archive),
     url(r'^breathing/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/$', views.breathing_month_archive),
     url(r'^breathing/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2})/$', views.breathing_day_archive),
